c0e2bdb207c298b077d1d54e2a995f30efb1c168
Stephen Wakefield <wakefields49@yahoo.com>
2017-08-07 00:20:57 -0500

    Update bingo.php

--- bingo.php
+++ bingo.php
@@ -1,6 +1,6 @@
 <?php
 $bingoValuesArray = [];
-$mathArray = [];
+$minRange = [];
 $numberOfColumns = 5;
 $rangeDiff = 15;
 $freeSpaceLocation = 2;
@@ -15,11 +15,11 @@ function randomArray($condition,$min,$max) {
 }
 
 for ($j=-1; $j < $numberOfColumns-1; $j++) { 
-	$mathArray[] = ($rangeDiff * $j) + $rangeDiff + 1;
+	$minRange[] = ($rangeDiff * $j) + $rangeDiff + 1;
 }
 
 for ($i=0; $i < $numberOfColumns; $i++) {
-	randomArray(($numberOfColumns * $i) + $numberOfColumns,$mathArray[$i],($rangeDiff * $i) + $rangeDiff);
+	randomArray(($numberOfColumns * $i) + $numberOfColumns,$minRange[$i],($rangeDiff * $i) + $rangeDiff);
 }
 
 $bingoValuesArray = array_chunk($bingoValuesArray,$numberOfColumns);
@@ -34,47 +34,46 @@ $bingoValuesArray[$freeSpaceLocation][$freeSpaceLocation]="Free Space";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bingo</title>
+	<style>
+	html, body { 
+		height: 100%; 
+		width: 100%;
+	}
+	body div {
+		height: 80%;
+		width: 80%;
+		padding: 5%;
+	}
+	table {
+		height: 70%;
+		width: 80%;
+		float: center;
+		text-align: center;
+	}
+	td, tr {
+		width: 5%;
+		background: #00FFFF;
+		font-size: 20px;
+		border: 1px solid black;
+	}
+	th {
+		background: #F5F5DC;
+		color: #00008B;
+		border: 1px solid black;
+	}
+	td:only-child {
+		background: white;
+		border: hidden;
+	}
+	button {
+		font-size: 15px;
+	}
+	a {
+		color: black;
+		text-decoration: none;
+	}
+	</style>
 </head>
-
-<style>
-html, body { 
-	height: 100%; 
-	width: 100%;
-}
-body div {
-	height: 80%;
-	width: 80%;
-	padding: 5%;
-}
-table {
-	height: 70%;
-	width: 80%;
-	float: center;
-	text-align: center;
-}
-td, tr {
-	width: 5%;
-	background: #00FFFF;
-	font-size: 20px;
-	border: 1px solid black;
-}
-th {
-	background: #F5F5DC;
-	color: #00008B;
-	border: 1px solid black;
-}
-td:only-child {
-	background: white;
-	border: hidden;
-}
-button {
-	font-size: 15px;
-}
-a {
-	color: black;
-	text-decoration: none;
-}
-</style>
 
 <body>
 	<div>
