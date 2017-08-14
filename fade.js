088747b9258db3750f279006928dbf3a64ed3aad
Stephen Wakefield <wakefields49@yahoo.com>
2017-08-14 10:50:04 -0500

    Add files via upload

---
+++ fade.js
@@ -0,0 +1,7 @@
+jQuery("#primary h1").css({"display":"none"});
+jQuery("#primary h2").css({"display":"none"});
+
+jQuery(document).ready(function(){
+	jQuery("#primary h1").fadeIn(2000);
+	jQuery("#primary h2").fadeIn(3000);
+});
